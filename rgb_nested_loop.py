0de0e26d2eef5eebd18871f0af235ca137b88b12
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-04-26 16:54:16 +0000

    comments

--- rgb_nested_loop.py
+++ rgb_nested_loop.py
@@ -4,14 +4,15 @@
 
 # Color code program in python
 
-
 import time
 
 def main():
+    # loop through every rgb colour code
     for red in range(0,270,15):
         for green in range(0,270,15):
             for blue in range(0,270,15):
                 time.sleep(.001)
+                # print out the colour code, with it formatted so that it shows the actual colour
                 print("\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(red, green, blue, "RGB(" + str(red) + "," + str(green) + "," + str(blue) + ")"))
 
 if __name__ == "__main__":
